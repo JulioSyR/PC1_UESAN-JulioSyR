d671b64666aa77925aac569a016bf6ce2d49a321
JulioSyR <75438915+JulioSyR@users.noreply.github.com>
2021-09-18 12:59:49 -0500

    Agregar archivos de proyecto.

---
+++ PC1_UESAN/Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC1_UESAN.Models
+{
+    public class Customer
+    {
+        private int id;
+        private string email;
+        private string fullName;
+        private string country;
+        private string phone;
+
+    }
+}
---
+++ PC1_UESAN/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PC1_UESAN.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
